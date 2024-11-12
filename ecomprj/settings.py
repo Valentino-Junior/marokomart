130f3c36f6a9cb04ca82393c26dbd987981727f6
Valentino Junior <ojvalentine14@gmail.com>
2024-11-12 20:52:35 +0300

    Added styling

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -148,6 +148,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+
+CSRF_TRUSTED_ORIGINS = ["https://effe-2c0f-fe38-202e-e83d-fcc6-2205-3f43-590b.ngrok-free.app"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
